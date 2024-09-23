8e0ef6967fbcc207f525184e74846718196a3840
Francisco Vicente Suárez Bellón <95106875+FVSB@users.noreply.github.com>
2024-09-22 20:19:41 -0400

    refactor greedy

--- Greedy/src/solucion_dp.py
+++ Greedy/src/solucion_dp.py
@@ -1,22 +1,26 @@
 from collections import deque
-import sys
+
 t = int(input())
 for _ in range(t):
-#n, m, k = map(int,sys.stdin.readline().split()) Codeforces
-n, m, k = map(int, input().split())
-#s = list(sys.stdin.readline().split()) Codeforces
-s = input
-dp = [-1] * (n + 2)
- dp[0] =
- for i in range(1, n + 2):
- if i != n + 1 and s[i - 1] == 'C':
- contin
- for t in range(1, m + 1):
- if i - t >= 0 and (i - t == 0 or s[i - t - 1] == 'L'):
- dp[i] = max(dp[i], dp[i - t
- if i > 1 and s[i - 2] == 'W':
- dp[i] = max(dp[i], dp[i - 1] - 
- if dp[n + 1] >= 0:
- print("YES")
- else:
- print("NO")
+    n, m, k = map(int, input().split())
+    s = input()
+    #n, m, k = map(int,sys.stdin.readline().split()) Codeforces
+    #s = list(sys.stdin.readline().split()) Codeforces
+    dp = [-1] * (n + 2)
+    dp[0] = k
+    
+    for i in range(1, n + 2):
+        if i != n + 1 and s[i - 1] == 'C':
+            continue
+        
+        for t in range(1, m + 1):
+            if i - t >= 0 and (i - t == 0 or s[i - t - 1] == 'L'):
+                dp[i] = max(dp[i], dp[i - t])
+        
+        if i > 1 and s[i - 2] == 'W':
+            dp[i] = max(dp[i], dp[i - 1] - 1)
+    
+    if dp[n + 1] >= 0:
+        print("YES")
+    else:
+        print("NO")
--- Greedy/src/solucion_greedy.py
+++ Greedy/src/solucion_greedy.py
@@ -1,26 +1,27 @@
 def run() -> None:
     n,m,k = map(int, input().split())
     A = input()
-    logs = [i for i in range(n) if A[i] == "L"]
-    logs.append(n+1)
+    logs = [i for i in range(n) if A[i] == "L"] # Enumera donde están los troncos
+    logs.append(n+1) # Añade el final como si fuera un tronco
     i = -1
     next_log = 0
-    while i < n-1:
-        if m >= logs[next_log] - i:
-            i = logs[next_log]
+    while i < n-1:# Ciclo while pq se irá saltando através de los troncos
+        if m >= logs[next_log] - i:# Si el siguiente tronco esta a a una distancia cruzable se salta a el
+            i = logs[next_log] # actualizar i hasta el tronco mas cercano
         else:
-            i+=m
-            if i > n-1:
+            i+=m # Saltar hasta el lugar saltable más cercano
+            if i > n-1:# Si estoy en la orilla puedo decir que es cruzable
                 print("YES")
                 return
-            while i < n and i < logs[next_log]:
+            while i < n and i < logs[next_log]: # Mientras i no este en la orilla y ademas no este en el siguiente tronco a nadar
                 if A[i] != "C" and k > 0:
                     i+=1
                     k-=1
-                else:
+                else:# Si no puedo nadar o hay un cocodrilo que no puedo saltar digo que no se puede realizar el salto
                     print("NO")
                     return
-        next_log +=1
+        next_log +=1 # Voy hasta el siguiente tronco
     print("YES")
  
 for _ in range(int(input())):
+    run()
