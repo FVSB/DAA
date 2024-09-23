d9555c8373f09c4d41d36c785297a13f4f7cf7db
Francisco Vicente Suárez Bellón <95106875+FVSB@users.noreply.github.com>
2024-09-22 20:40:17 -0400

    update dp solution in greedy problem

--- Greedy/src/solucion_dp.py
+++ Greedy/src/solucion_dp.py
@@ -9,15 +9,15 @@ for _ in range(t):
     dp = [-1] * (n + 2)
     dp[0] = k
     
-    for i in range(1, n + 2):
-        if i != n + 1 and s[i - 1] == 'C':
+    for i in range(1, n + 2):# De primera casilla a orilla
+        if i != n + 1 and s[i - 1] == 'C': # Si esto en la orilla derecha o hay un cocodrilo no actualizo
             continue
         
-        for t in range(1, m + 1):
+        for t in range(1, m + 1):# Trato de actualiza si hubo un tronco  o la orilla izq anterior a mi
             if i - t >= 0 and (i - t == 0 or s[i - t - 1] == 'L'):
                 dp[i] = max(dp[i], dp[i - t])
         
-        if i > 1 and s[i - 2] == 'W':
+        if i > 1 and s[i - 2] == 'W': 
             dp[i] = max(dp[i], dp[i - 1] - 1)
     
     if dp[n + 1] >= 0:
